82f2b51328647ba522a3a9624dc319c716d8cfdc
jonat70 <Jonathan.poi@escola.pr.gov.br>
2026-06-05 15:08:25 -0300

    Add alert message on button click

---
+++ script.js
@@ -0,0 +1,7 @@
+const botao = document.getElementById("btnMensagem");
+
+botao.addEventListener("click", () => {
+    alert(
+        "O futuro do agronegócio depende do equilíbrio entre produtividade, inovação e preservação ambiental."
+    );
+});
